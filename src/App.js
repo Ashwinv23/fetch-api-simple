e54c3cecb406b78d1c57313a870ce9736b23398f
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-03-29 17:37:18 +0530

    added useEffect fn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import { useState, useEffect } from "react";
 import Form from "./Form";
 
 function App() {
+  const API_URL = "https://jsonplaceholder.typicode.com/";
+  const [items, setItems] = useState([]);
+  const [reqType, setReqType] = useState("users");
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        console.log(data);
+        setItems(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="App">
       <Form />
